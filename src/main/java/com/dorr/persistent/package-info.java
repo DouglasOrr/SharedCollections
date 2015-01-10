aebdcb69f2b7d6f732cd84431abdaba40c335775
Douglas Orr <douglas.orr99@gmail.com>
2015-01-10 17:39:30 +0000

    Minor: tweak wording

--- src/main/java/com/dorr/persistent/package-info.java
+++ src/main/java/com/dorr/persistent/package-info.java
@@ -1,5 +1,5 @@
 /**
- * A simple collection of persistent data structures for use in plain old Java code.
+ * A small collection of persistent data structures for use in plain old Java code.
  * <h2>Background</h2>
  * <p>An implementation of the same data structures that functional JVM languages such
  * as Clojure have, designed for use in plain Java code.</p>
