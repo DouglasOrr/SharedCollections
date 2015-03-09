0a8209ef2608743cef4fcd0e919bf997815592a3
Douglas Orr <douglas.orr99@gmail.com>
2015-03-09 22:21:16 +0000

    Add a first graph, to get us underway

--- js/sharedcollections.js
+++ js/sharedcollections.js
@@ -12,6 +12,10 @@ function getVersions(handler) {
     });
 }
 
+function getProfile(version, handler) {
+    $.get("releases/" + version + "/profile/profile.json", handler);
+}
+
 getVersions(function(versions) {
     // update all the dependency panels to show the latest version number
     $(".deps").text(function(i,text) {
@@ -19,6 +23,33 @@ getVersions(function(versions) {
     });
     $("pre code.deps").each(function(i,block) {
         hljs.highlightBlock(block);
+    });
+    getProfile(versions[0], function(data) {
+        var results = data.profile["Map.put"];
+        var series = [];
+        for (var impl in results) {
+            series.push({
+                key: impl,
+                values: results[impl].map(function(o) {
+                    return {x: o.size,
+                            y: o.latency_ns / (o.repetitions * o.size)};
+                })});
+        }
+
+        nv.addGraph(function() {
+            var chart = nv.models.lineChart()
+                .xScale(d3.scale.log().base(2));
+            chart.xAxis.axisLabel("Peak collection size")
+                .showMaxMin(false);
+            chart.yAxis.axisLabel("Average operation time /ns")
+                .showMaxMin(false);
+            d3.select('#performance-chart svg')
+                .datum(series)
+                .call(chart);
+            nv.utils.windowResize(chart.update);
+            chart.update();
+            return chart;
+        });
     });
 });
 
